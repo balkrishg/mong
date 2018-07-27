0198fb07cb7c97041303c0feb15485924899639b
babluchandran <bc00434278@techmahindra.com>
2018-07-27 12:20:58 +0000

    Update SpringBootMongoDbApplication.java

--- src/main/java/com/dineshonjava/sbmdb/SpringBootMongoDbApplication.java
+++ src/main/java/com/dineshonjava/sbmdb/SpringBootMongoDbApplication.java
@@ -2,11 +2,10 @@ package com.dineshonjava.sbmdb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+@SpringBootApplication
+
 public class SpringBootMongoDbApplication {
 
 	public static void main(String[] args) {
